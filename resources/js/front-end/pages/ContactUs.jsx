e025b466613fcb073822db7545b04b973bde7133
Simone823 <simone.daglio823@outlook.it>
2024-01-07 17:32:33 +0100

    nav

---
+++ resources/js/front-end/pages/ContactUs.jsx
@@ -0,0 +1,15 @@
+/*** IMPORTS ***/
+import React, {useEffect} from 'react';
+/***************************************** */
+
+const ContactUs = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <div>ContactUs</div>
+    );
+}
+
+export default ContactUs;
