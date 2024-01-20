868c57666d92955d2c9c24f00f07fa1643500320
Simone823 <simone.daglio823@outlook.it>
2024-01-20 12:06:09 +0100

    fix delete modal profiles

--- resources/views/user/profiles/deleteModal.blade.php
+++ resources/views/user/profiles/deleteModal.blade.php
@@ -1,14 +1,14 @@
 {{-- Button --}}
-<button type="button" class="btn btn-danger text-uppercase" data-bs-toggle="modal" data-bs-target="#deleteUserAccountModal">
+<button type="button" class="btn btn-danger text-uppercase" data-bs-toggle="modal" data-bs-target="#deleteUserModal">
     Elimina Account
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="deleteUserAccountModal" tabindex="-1" aria-labelledby="deleteUserAccountModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteUserModal" tabindex="-1" aria-labelledby="deleteUserModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content bg-body-secondary">
             <div class="modal-header border-0">
-                <h1 class="modal-title text-danger fw-bold fs-5" id="deleteUserAccountModalLabel">
+                <h1 class="modal-title text-danger fw-bold fs-5" id="deleteUserModalLabel">
                     Elimina Account
                 </h1>
                 <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
