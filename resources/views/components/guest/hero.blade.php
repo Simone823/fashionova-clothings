b97bbcf8f7aa0f1bf1a4d8447700c1df15b3ef2c
Simone823 <simone.daglio823@outlook.it>
2024-02-18 15:37:02 +0100

    refactoring hero guest

--- resources/views/components/guest/hero.blade.php
+++ resources/views/components/guest/hero.blade.php
@@ -3,11 +3,11 @@
         <div class='row'>
             <div class='col-12 col-md-6'>
                 <h1>
-                    Esplora il tuo stile unico con Fashionova Clothings.
-                    Scopri la moda che parla di te. Benvenuto in un mondo di eleganza senza tempo. 
+                    Scopri la tua moda unica con Fashionova Clothings.
+                    Trova lo stile che ti distingue. Benvenuto in un mondo di eleganza senza tempo.
                 </h1>
                 <a href='{{route('guest.products.shop')}}' class='btn btn-primary'>
-                    Esplora la collezione
+                    Scopri la collezione
                 </a>
             </div>
         </div>
