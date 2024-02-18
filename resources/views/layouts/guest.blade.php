486f14d07023bf1bc8d90020b019022e570c0ffb
Simone823 <simone.daglio823@outlook.it>
2024-02-18 13:38:11 +0100

    refactoring layouts guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,26 +7,26 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- TItle -->
     <title>{{ config('app.name', 'Laravel') }} | @yield('metaTitle')</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/guest.js') }}" defer></script>
-    @stack('javascript')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Styles -->
     <link href="{{ asset('css/guest.css') }}" rel="stylesheet">
 
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Scripts -->
+    <script src="{{ asset('js/guest.js') }}" defer></script>
+    @stack('javascript')
 </head>
 <body>
 
     <div id="app"> 
-        
         {{-- navbar --}}
         @include('components.guest.navbar')
 
